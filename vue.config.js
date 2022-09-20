3f921d46a144d73b11fa4c064367dc8a761a4423
Bassel Aflak <b7a@hotmail.de>
2022-09-20 20:21:49 +0300

    fixed locale, title and icon

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,12 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
   ? '/advice-generator-app/'
-  : '/'
+  : '/',
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      title: 'Advice Me',
+    },
+  }
 })
