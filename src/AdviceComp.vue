df78332df131c4b5248721e236233ab2c3b99a6a
Bassel Aflak <b7a@hotmail.de>
2022-09-20 20:41:58 +0300

    added bsic html structure

--- src/AdviceComp.vue
+++ src/AdviceComp.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="">
+        <h1>Advice #{{myAdvice.id}}</h1>
         <p>{{myAdvice.advice}}</p>
+        <picture>
+            <source media="(min-width: 992px)" :srcset="publicPath+'images/pattern-divider-desktop.svg'">
+            <img :src="publicPath+'images/pattern-divider-mobile.svg'" alt="Pattern Divider">
+        </picture>
+        <div>
+            <label for="random_advice" class="screen-reader">Get Random Advice</label>
+            <button class="randomize" type="button" id="random_advice">
+                <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z" fill="#202733"/>
+                </svg>
+            </button>
+        </div>
   </div>
 </template>
 
@@ -12,7 +25,7 @@ export default {
     name:'AdviceComp',
     setup() {
         const useAdvice = advice()
-        
+        const publicPath = process.env.BASE_URL
 
         onMounted(() => {
             useAdvice.getRandomAdvice()
@@ -21,7 +34,8 @@ export default {
         return {
             myAdvice: useAdvice.advice, 
             errors: useAdvice.errors, 
-            getRandomAdvice: useAdvice.getRandomAdvice
+            getRandomAdvice: useAdvice.getRandomAdvice,
+            publicPath
         }
     }
 }
